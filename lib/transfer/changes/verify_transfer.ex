1b2b00edd19f9ae66fba7882165476b5fef8c7e7
Zach Daniel <zach@zachdaniel.dev>
2024-06-23 20:04:47 -0400

    fix: better validations around atomics improvement: allow skipping balance updates on request

--- lib/transfer/changes/verify_transfer.ex
+++ lib/transfer/changes/verify_transfer.ex
@@ -9,8 +9,10 @@ defmodule AshDoubleEntry.Transfer.Changes.VerifyTransfer do
   require Ash.Query
 
   def atomic(changeset, opts, context) do
-    if AshDoubleEntry.Transfer.Info.transfer_destroy_balances?(changeset.resource) ||
-         Ash.Changeset.changing_attribute?(changeset, :amount) do
+    if (AshDoubleEntry.Transfer.Info.transfer_destroy_balances?(changeset.resource) &&
+          changeset.action.type == :destroy) ||
+         (Ash.Changeset.changing_attribute?(changeset, :amount) &&
+            !changeset.context[:ash_double_entry][:skip_balance_updates]) do
       {:error,
        "Cannot destroy a transfer atomically if balances must be destroyed manually, or if balance is being updated"}
     else
@@ -19,165 +21,169 @@ defmodule AshDoubleEntry.Transfer.Changes.VerifyTransfer do
   end
 
   def change(changeset, _opts, context) do
-    if changeset.action.type == :update and
-         Enum.any?(
-           [:from_account_id, :to_account_id, :id],
-           &Ash.Changeset.changing_attribute?(changeset, &1)
-         ) do
-      Ash.Changeset.add_error(
-        changeset,
-        "Cannot modify a transfer's from_account_id, to_account_id, or id"
-      )
+    if changeset.context[:ash_double_entry][:skip_balance_updates] do
+      changeset
     else
-      if changeset.action.type == :create do
-        timestamp = Ash.Changeset.get_attribute(changeset, :timestamp)
-
-        timestamp =
-          case timestamp do
-            nil -> System.system_time(:millisecond)
-            timestamp -> DateTime.to_unix(timestamp, :millisecond)
-          end
-
-        ulid = AshDoubleEntry.ULID.generate(timestamp)
-
-        Ash.Changeset.force_change_attribute(changeset, :id, ulid)
+      if changeset.action.type == :update and
+           Enum.any?(
+             [:from_account_id, :to_account_id, :id],
+             &Ash.Changeset.changing_attribute?(changeset, &1)
+           ) do
+        Ash.Changeset.add_error(
+          changeset,
+          "Cannot modify a transfer's from_account_id, to_account_id, or id"
+        )
       else
-        changeset
-      end
-      |> maybe_destroy_balances(context)
-      |> Ash.Changeset.after_action(fn changeset, result ->
-        from_account_id = Ash.Changeset.get_attribute(changeset, :from_account_id)
-        to_account_id = Ash.Changeset.get_attribute(changeset, :to_account_id)
-        new_amount = Ash.Changeset.get_attribute(changeset, :amount)
+        if changeset.action.type == :create do
+          timestamp = Ash.Changeset.get_attribute(changeset, :timestamp)
 
-        old_amount =
-          if changeset.action.type == :destroy do
-            Money.new!(0, new_amount.currency)
-          else
-            case changeset.data do
-              %{amount: nil} ->
-                Money.new!(0, new_amount.currency)
-
-              %Ash.Changeset.OriginalDataNotAvailable{} ->
-                Money.new!(0, new_amount.currency)
-
-              %{amount: amount} ->
-                amount
+          timestamp =
+            case timestamp do
+              nil -> System.system_time(:millisecond)
+              timestamp -> DateTime.to_unix(timestamp, :millisecond)
             end
-          end
 
-        amount_delta =
-          Money.sub!(new_amount, old_amount)
+          ulid = AshDoubleEntry.ULID.generate(timestamp)
 
-        accounts =
-          changeset.resource
-          |> AshDoubleEntry.Transfer.Info.transfer_account_resource!()
-          |> Ash.Query.filter(id in ^[from_account_id, to_account_id])
-          |> Ash.Query.set_context(%{ash_double_entry?: true})
-          |> Ash.Query.for_read(
-            :lock_accounts,
-            %{},
-            Ash.Context.to_opts(context,
-              authorize?: authorize?(changeset.domain),
-              domain: changeset.domain
-            )
-          )
-          |> Ash.Query.load(balance_as_of_ulid: %{ulid: result.id})
-          |> Ash.read!()
+          Ash.Changeset.force_change_attribute(changeset, :id, ulid)
+        else
+          changeset
+        end
+        |> maybe_destroy_balances(context)
+        |> Ash.Changeset.after_action(fn changeset, result ->
+          from_account_id = Ash.Changeset.get_attribute(changeset, :from_account_id)
+          to_account_id = Ash.Changeset.get_attribute(changeset, :to_account_id)
+          new_amount = Ash.Changeset.get_attribute(changeset, :amount)
 
-        from_account = Enum.find(accounts, &(&1.id == from_account_id))
-        to_account = Enum.find(accounts, &(&1.id == to_account_id))
+          old_amount =
+            if changeset.action.type == :destroy do
+              Money.new!(0, new_amount.currency)
+            else
+              case changeset.data do
+                %{amount: nil} ->
+                  Money.new!(0, new_amount.currency)
 
-        new_from_account_balance =
-          Money.sub!(
-            from_account.balance_as_of_ulid || Money.new!(0, from_account.currency),
-            amount_delta
-          )
+                %Ash.Changeset.OriginalDataNotAvailable{} ->
+                  Money.new!(0, new_amount.currency)
 
-        new_to_account_balance =
-          Money.add!(
-            to_account.balance_as_of_ulid || Money.new!(0, to_account.currency),
-            amount_delta
-          )
+                %{amount: amount} ->
+                  amount
+              end
+            end
 
-        balance_resource =
-          AshDoubleEntry.Transfer.Info.transfer_balance_resource!(changeset.resource)
+          amount_delta =
+            Money.sub!(new_amount, old_amount)
 
-        bulk_destroy_errors =
-          unless changeset.action.type == :destroy do
-            Ash.bulk_create(
-              [
-                %{
-                  account_id: from_account.id,
-                  transfer_id: result.id,
-                  balance: new_from_account_balance
-                },
-                %{
-                  account_id: to_account.id,
-                  transfer_id: result.id,
-                  balance: new_to_account_balance
-                }
-              ],
-              balance_resource,
-              :upsert_balance,
+          accounts =
+            changeset.resource
+            |> AshDoubleEntry.Transfer.Info.transfer_account_resource!()
+            |> Ash.Query.filter(id in ^[from_account_id, to_account_id])
+            |> Ash.Query.set_context(%{ash_double_entry?: true})
+            |> Ash.Query.for_read(
+              :lock_accounts,
+              %{},
               Ash.Context.to_opts(context,
-                domain: changeset.domain,
-                upsert_fields: [:balance],
-                return_errors?: true,
-                stop_on_error?: true
+                authorize?: authorize?(changeset.domain),
+                domain: changeset.domain
               )
             )
-          end
-          |> case do
-            nil ->
-              []
+            |> Ash.Query.load(balance_as_of_ulid: %{ulid: result.id})
+            |> Ash.read!()
 
-            %Ash.BulkResult{status: :success} ->
-              []
+          from_account = Enum.find(accounts, &(&1.id == from_account_id))
+          to_account = Enum.find(accounts, &(&1.id == to_account_id))
 
-            %Ash.BulkResult{errors: errors} ->
-              errors
-          end
+          new_from_account_balance =
+            Money.sub!(
+              from_account.balance_as_of_ulid || Money.new!(0, from_account.currency),
+              amount_delta
+            )
 
-        case bulk_destroy_errors do
-          [] ->
-            amount_delta =
-              if changeset.action.type == :destroy do
-                Money.mult!(amount_delta, -1)
-              else
-                amount_delta
+          new_to_account_balance =
+            Money.add!(
+              to_account.balance_as_of_ulid || Money.new!(0, to_account.currency),
+              amount_delta
+            )
+
+          balance_resource =
+            AshDoubleEntry.Transfer.Info.transfer_balance_resource!(changeset.resource)
+
+          bulk_destroy_errors =
+            unless changeset.action.type == :destroy do
+              Ash.bulk_create(
+                [
+                  %{
+                    account_id: from_account.id,
+                    transfer_id: result.id,
+                    balance: new_from_account_balance
+                  },
+                  %{
+                    account_id: to_account.id,
+                    transfer_id: result.id,
+                    balance: new_to_account_balance
+                  }
+                ],
+                balance_resource,
+                :upsert_balance,
+                Ash.Context.to_opts(context,
+                  domain: changeset.domain,
+                  upsert_fields: [:balance],
+                  return_errors?: true,
+                  stop_on_error?: true
+                )
+              )
+            end
+            |> case do
+              nil ->
+                []
+
+              %Ash.BulkResult{status: :success} ->
+                []
+
+              %Ash.BulkResult{errors: errors} ->
+                errors
+            end
+
+          case bulk_destroy_errors do
+            [] ->
+              amount_delta =
+                if changeset.action.type == :destroy do
+                  Money.mult!(amount_delta, -1)
+                else
+                  amount_delta
+                end
+
+              Ash.bulk_update(
+                balance_resource,
+                :adjust_balance,
+                %{
+                  from_account_id: from_account.id,
+                  to_account_id: to_account.id,
+                  transfer_id: result.id,
+                  delta: amount_delta
+                },
+                Ash.Context.to_opts(context,
+                  domain: changeset.domain,
+                  strategy: [:atomic, :stream, :atomic_batches],
+                  return_errors?: true,
+                  stop_on_error?: true
+                )
+                |> Keyword.update(
+                  :context,
+                  %{ash_double_entry?: true},
+                  &Map.put(&1, :ash_double_entry?, true)
+                )
+              )
+              |> case do
+                %Ash.BulkResult{status: :success} -> {:ok, result}
+                %Ash.BulkResult{errors: errors} -> {:error, errors}
               end
 
-            Ash.bulk_update(
-              balance_resource,
-              :adjust_balance,
-              %{
-                from_account_id: from_account.id,
-                to_account_id: to_account.id,
-                transfer_id: result.id,
-                delta: amount_delta
-              },
-              Ash.Context.to_opts(context,
-                domain: changeset.domain,
-                strategy: [:atomic, :stream, :atomic_batches],
-                return_errors?: true,
-                stop_on_error?: true
-              )
-              |> Keyword.update(
-                :context,
-                %{ash_double_entry?: true},
-                &Map.put(&1, :ash_double_entry?, true)
-              )
-            )
-            |> case do
-              %Ash.BulkResult{status: :success} -> {:ok, result}
-              %Ash.BulkResult{errors: errors} -> {:error, errors}
-            end
-
-          errors ->
-            {:error, errors}
-        end
-      end)
+            errors ->
+              {:error, errors}
+          end
+        end)
+      end
     end
   end
 
