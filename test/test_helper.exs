eb635d67bd1463bcd1b6cfddc505f6060262eaa8
James Harton <james@harton.nz>
2025-10-15 09:04:14 +1300

    chore: Fix REUSE copyright attributions

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2020 Zach Daniel
+# SPDX-FileCopyrightText: 2023 ash_double_entry contributors <https://github.com/ash-project/ash_double_entry/graphs.contributors>
 #
 # SPDX-License-Identifier: MIT
 
