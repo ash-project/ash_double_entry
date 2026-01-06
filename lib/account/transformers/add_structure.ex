eece747851d4398095c31f83e21a0488ec20dd2a
Simon Bergström <simon.bergstrom@erkols-studios.se>
2026-01-06 11:34:47 +0100

    fix(#150): added `before?/1` clauses to the three transformers for `SetRelationshipSource`
    Each `AddStructure` transformer will execute before the
    `Ash.Resource.Transformers.SetRelationshipSource`.

--- lib/account/transformers/add_structure.ex
+++ lib/account/transformers/add_structure.ex
@@ -8,6 +8,7 @@ defmodule AshDoubleEntry.Account.Transformers.AddStructure do
   use Spark.Dsl.Transformer
   import Spark.Dsl.Builder
 
+  def before?(Ash.Resource.Transformers.SetRelationshipSource), do: true
   def before?(Ash.Resource.Transformers.BelongsToAttribute), do: true
   def before?(Ash.Resource.Transformers.CachePrimaryKey), do: true
   def before?(_), do: false
